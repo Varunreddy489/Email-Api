1c32b60f95772eab2aac465c273697687905c72e
Varunreddy <varunsannapureddy@gmail.com>
2024-12-08 01:24:08 +0530

    updated dependencies

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import nodemailer from "nodemailer";
-import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
